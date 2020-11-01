398b72af4686d778056b2f39013f54b10e93d74a
chris3764 <69045948+chris3764@users.noreply.github.com>
2020-11-01 13:37:23 -0500

    Basic Structure of Orders Page
    Put lists and on orders page and moved list down to put text for the categories

--- swampy/lib/pages/orders_page.dart
+++ swampy/lib/pages/orders_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:swampy/components/list/list_element.dart';
+import 'package:swampy/components/menus/nav_bar.dart';
+import 'package:swampy/pages/orders_page/orders_page_desktop.dart';
+import 'package:swampy/pages/orders_page/orders_page_mobile.dart';
+import 'package:swampy/pages/orders_page/orders_page_tablet.dart';
 
 class OrdersPage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  List<ListElement> sample = List.generate(
+      25,
+      (index) => ListElement(
+            items: ['$index', '${index + 7}', '${index + 8}', '${index + 9}'],
+          ));
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Orders'),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.white,
+          key: _scaffoldKey,
+          body: NotificationListener<OverscrollIndicatorNotification>(
+              onNotification: (OverscrollIndicatorNotification overscroll) {
+                overscroll.disallowGlow();
+                return;
+              },
+              child: ScreenTypeLayout(
+                desktop: OrdersPageDesktop(sample: sample),
+                tablet: OrdersPageTablet(sample: sample),
+                mobile: OrdersPageMobile(sample: sample),
+              ))),
     );
   }
 }
---
+++ swampy/lib/pages/orders_page/orders_page_tablet.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:swampy/components/list/list_element.dart';
+import 'package:swampy/components/menus/nav_bar.dart';
+import 'package:swampy/components/menus/side_menu.dart';
+
+class OrdersPageTablet extends StatelessWidget {
+  final List<ListElement> sample;
+
+  OrdersPageTablet({this.sample});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.0),
+        child: NavBar(),
+      ),
+      body: SafeArea(
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: SideMenu(),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 200,
+              height: MediaQuery.of(context).size.height - 100,
+              child: ListView.builder(
+                itemCount: sample.length,
+                itemBuilder: (context, index) => sample[index],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
