799559ef6020c785e3eb8012392b29af62b9f231
Fabio Suarez <fabiosuarez.o0@gmail.com>
2020-11-01 19:02:59 -0500

    Update list_wrapper.dart

--- swampy/lib/components/list/list_wrapper.dart
+++ swampy/lib/components/list/list_wrapper.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class ListWrapper extends StatefulWidget {
+  @override
+  _ListWrapperState createState() => _ListWrapperState();
+}
+
+class _ListWrapperState extends State<ListWrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
