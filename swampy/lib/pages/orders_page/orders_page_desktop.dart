180f5a914a32fe75baa2dc63c977d4b1ea924398
Fabio Suarez <fabiosuarez.o0@gmail.com>
2020-11-01 20:10:04 -0500

    thing

--- swampy/lib/pages/orders_page/orders_page_desktop.dart
+++ swampy/lib/pages/orders_page/orders_page_desktop.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:swampy/components/general/column_builder.dart';
+import 'package:swampy/components/general/section.dart';
 import 'package:swampy/components/list/list_element.dart';
 import 'package:swampy/components/menus/nav_bar.dart';
 import 'package:swampy/components/menus/side_menu.dart';
@@ -28,23 +29,26 @@ class OrdersPageDesktop extends StatelessWidget {
             ),
             Container(
               width: MediaQuery.of(context).size.width - 200,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text('First'),
-                        Text('Second'),
-                        Text('Third'),
-                        Text('Fourth'),
-                      ],
-                    ),
-                    ColumnBuilder(
-                      itemCount: sample.length,
-                      itemBuilder: (context, index) => sample[index],
-                    ),
-                  ],
+              child: Section(
+                title: 'Products',
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('First'),
+                          Text('Second'),
+                          Text('Third'),
+                          Text('Fourth'),
+                        ],
+                      ),
+                      ColumnBuilder(
+                        itemCount: sample.length,
+                        itemBuilder: (context, index) => sample[index],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             )
