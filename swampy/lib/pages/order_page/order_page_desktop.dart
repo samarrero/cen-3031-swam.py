9740c02d7a836601fed25d1cb829af8315c261ed
karlaalonsoUF <70971795+karlaalonsoUF@users.noreply.github.com>
2020-11-04 17:45:51 -0500

    start of building card in order page

--- swampy/lib/pages/order_page/order_page_desktop.dart
+++ swampy/lib/pages/order_page/order_page_desktop.dart
@@ -3,6 +3,7 @@ import 'package:swampy/components/general/column_builder.dart';
 import 'package:swampy/components/list/list_element.dart';
 import 'package:swampy/components/menus/nav_bar.dart';
 import 'package:swampy/components/menus/side_menu.dart';
+import 'package:swampy/components/general/row_builder.dart';
 
 class OrderPageDesktop extends StatelessWidget {
   final List<ListElement> sample;
@@ -26,6 +27,38 @@ class OrderPageDesktop extends StatelessWidget {
                 child: SideMenu(),
               ),
             ),
+            Container(
+                width: MediaQuery.of(context).size.width - 200,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0)),
+                  elevation: 3.0,
+                  child: Row(children: <Widget>[
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          const ListTile(
+                              //title: Text('Order #', style: Theme.of(context).textTheme.bodyText2),
+                              ),
+                          const ListTile(
+                            title: Text('Date'),
+                          ),
+                          const ListTile(
+                            title: Text('Status'),
+                          ),
+                          const ListTile(
+                            title: Text('Total'),
+                          ),
+                        ]),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          const ListTile(
+                            title: Text('Customer Info'),
+                          ),
+                        ])
+                  ]),
+                )),
             Container(
               width: MediaQuery.of(context).size.width - 200,
               child: ListView.builder(
