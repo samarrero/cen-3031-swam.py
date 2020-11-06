878716cf1974f581616069000ff63e244abac941
chris3764 <69045948+chris3764@users.noreply.github.com>
2020-11-06 18:01:01 -0500

    Order page card and added list wrapper to bottom

--- swampy/lib/pages/order_page.dart
+++ swampy/lib/pages/order_page.dart
@@ -5,6 +5,7 @@ import 'package:swampy/components/menus/nav_bar.dart';
 import 'package:swampy/pages/order_page/order_page_desktop.dart';
 import 'package:swampy/pages/order_page/order_page_mobile.dart';
 import 'package:swampy/pages/order_page/order_page_tablet.dart';
+import 'package:swampy/models/product.dart';
 
 class OrderPage extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -17,6 +18,88 @@ class OrderPage extends StatelessWidget {
       (index) => ListElement(
             items: ['$index', '${index + 4}', '${index + 8}', '${index + 12}'],
           ));
+  List<Product> products = [
+    Product(
+        name: 'A',
+        vendor: 'A',
+        amountInInventory: 5,
+        type: 'A',
+        amountSold: 1,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'B',
+        vendor: 'B',
+        amountInInventory: 2,
+        type: 'B',
+        amountSold: 21,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'C',
+        vendor: 'E',
+        amountInInventory: 3,
+        type: 'C',
+        amountSold: 30,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'D',
+        vendor: 'G',
+        amountInInventory: 76,
+        type: 'G',
+        amountSold: 1,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'E',
+        vendor: 'F',
+        amountInInventory: 1,
+        type: 'BG',
+        amountSold: 22,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'F',
+        vendor: 'Z',
+        amountInInventory: 67,
+        type: 'C',
+        amountSold: 3,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'ASDF',
+        vendor: 'E',
+        amountInInventory: 3,
+        type: 'C',
+        amountSold: 32,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'OE EFSD',
+        vendor: 'G',
+        amountInInventory: 76,
+        type: 'G',
+        amountSold: 1,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'AKEHEF',
+        vendor: 'F',
+        amountInInventory: 1,
+        type: 'BG',
+        amountSold: 2,
+        id: 'no',
+        price: 0),
+    Product(
+        name: 'FAEYGF',
+        vendor: 'Z',
+        amountInInventory: 67,
+        type: 'C',
+        amountSold: 56,
+        id: 'no',
+        price: 0),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +114,7 @@ class OrderPage extends StatelessWidget {
                 return;
               },
               child: ScreenTypeLayout(
-                desktop: OrderPageDesktop(sample: sample),
+                desktop: OrderPageDesktop(products: products),
                 tablet: OrderPageTablet(sample: sample),
                 mobile: OrderPageMobile(sample: sample),
               ))),
--- swampy/lib/pages/order_page/order_page_desktop.dart
+++ swampy/lib/pages/order_page/order_page_desktop.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:swampy/components/general/column_builder.dart';
+import 'package:swampy/components/general/section.dart';
 import 'package:swampy/components/list/list_element.dart';
+import 'package:swampy/components/list/list_wrapper.dart';
 import 'package:swampy/components/menus/nav_bar.dart';
 import 'package:swampy/components/menus/side_menu.dart';
+import 'package:swampy/models/product.dart';
 import 'package:swampy/components/general/row_builder.dart';
 
 class OrderPageDesktop extends StatelessWidget {
-  final List<ListElement> sample;
-
-  OrderPageDesktop({this.sample});
+  final List<Product> products;
+  OrderPageDesktop({this.products});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +20,7 @@ class OrderPageDesktop extends StatelessWidget {
         child: NavBar(),
       ),
       body: SafeArea(
-          child: Row(
+        child: Row(
           children: [
             Container(
               width: 200,
@@ -36,31 +38,87 @@ class OrderPageDesktop extends StatelessWidget {
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(32.0)),
                       elevation: 3.0,
-                      child: Row(children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text('Order #', style: Theme.of(context).textTheme.bodyText2),
-                              Text('Date'),
-                              Text('Status'),
-                              Text('Total'),
-                            ]),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text('Customer Info'),
-                            ])
-                      ]),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Padding(
+                                padding:
+                                    EdgeInsets.symmetric(horizontal: 15.0)),
+                            Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 7.0)),
+                                  Text('Order #',
+                                      style:
+                                          Theme.of(context).textTheme.headline4,
+                                      textAlign: TextAlign.justify),
+                                  Padding(
+                                      padding: EdgeInsets.fromLTRB(
+                                          0.0, 7.0, 0.0, 0.0)),
+                                  Text('Date:',
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                      textAlign: TextAlign.justify),
+                                  Text('Status:',
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                      textAlign: TextAlign.justify),
+                                  Text('Total:',
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                      textAlign: TextAlign.justify),
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 7.0)),
+                                ]),
+                            // Padding(
+                            //     padding:
+                            //         EdgeInsets.symmetric(horizontal: 100.0)),
+                            // Column(
+                            //     crossAxisAlignment: CrossAxisAlignment.start,
+                            //     children: [
+                            //       Padding(
+                            //           padding: EdgeInsets.symmetric(
+                            //               vertical: 7.0, horizontal: 15.0)),
+                            //       Text('Customer Information',
+                            //           style:
+                            //               Theme.of(context).textTheme.headline5,
+                            //           textAlign: TextAlign.start),
+                            //       Padding(
+                            //           padding: EdgeInsets.symmetric(
+                            //               vertical: 7.0, horizontal: 15.0)),
+                            //     ]),
+                          ]),
                     ),
-                    ColumnBuilder(
-                      itemCount: sample.length,
-                      itemBuilder: (context, index) => sample[index],
-                    )
+                    Container(
+                      width: MediaQuery.of(context).size.width - 200,
+                      child: ListWrapper(
+                        titles: [
+                          'Product',
+                          'Inventory',
+                          'Type',
+                          'Vendor',
+                          '# Sold'
+                        ],
+                        elements: products
+                            .map((product) => ListElement(
+                                  items: [
+                                    product.name,
+                                    product.amountInInventory.toString(),
+                                    product.type,
+                                    product.vendor,
+                                    product.amountSold.toString()
+                                  ],
+                                ))
+                            .toList(),
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
-
           ],
         ),
       ),
