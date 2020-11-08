4b6b6860aa8750cd76ec4a8cb2462f784efc95aa
Samuel Marrero <helicopter1339@gmail.com>
2020-11-07 19:53:50 -0500

    made list element clickable

--- swampy/lib/components/list/list_element.dart
+++ swampy/lib/components/list/list_element.dart
@@ -10,23 +10,28 @@ class ListElement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(32.0)
-      ),
-      elevation: 3.0,
-      child: RowBuilder(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12.0),
-            child: Text(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32.0)
+        ),
+        elevation: 3.0,
+        child: InkWell(
+        onTap: () {
+      
+        },
+          child: RowBuilder(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              child: Text(
                 items[index],
                 style: Theme.of(context).textTheme.headline6,
-            ),
-          );
-        },
-      ),
+              ),
+            );
+          },
+        ),
+        ),
     );
   }
 }
