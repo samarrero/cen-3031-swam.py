0a19e9c67535117457ef1495d3b69bfebabcd4e4
Samuel Marrero <helicopter1339@gmail.com>
2020-11-08 21:36:49 -0500

    removed dashes from sample data's order numbers

--- swampy/lib/data/sample_data.dart
+++ swampy/lib/data/sample_data.dart
@@ -15,17 +15,17 @@ List<Product> sampleProducts = [
 ];
 
 List<Order> sampleOrders = [
-  Order(id: '00', orderNumber: '1234-44-332', date: DateTime(2020, 10, 9), productsAndAmount: {sampleProducts[0]: 4}, total: 120.21, fulfilled: true),
-  Order(id: '01', orderNumber: '1234-44-333', date: DateTime(2020, 10, 10), productsAndAmount: {sampleProducts[3]: 2, sampleProducts[4]: 1}, total: 17.38, fulfilled: true),
-  Order(id: '02', orderNumber: '1234-44-334', date: DateTime(2020, 10, 10), productsAndAmount: {sampleProducts[7]: 1, sampleProducts[1]: 3}, total: 86.45, fulfilled: true),
-  Order(id: '03', orderNumber: '1234-44-335', date: DateTime(2020, 10, 11), productsAndAmount: {sampleProducts[2]: 2, sampleProducts[0]: 1}, total: 60.49, fulfilled: true),
-  Order(id: '04', orderNumber: '1234-44-336', date: DateTime(2020, 10, 13), productsAndAmount: {sampleProducts[1]: 1, sampleProducts[2]: 3}, total: 38.48, fulfilled: false),
-  Order(id: '05', orderNumber: '1234-44-337', date: DateTime(2020, 10, 13), productsAndAmount: {sampleProducts[0]: 1, sampleProducts[8]: 1}, total: 58.39, fulfilled: true),
-  Order(id: '06', orderNumber: '1234-44-338', date: DateTime(2020, 10, 14), productsAndAmount: {sampleProducts[5]: 1, sampleProducts[6]: 2}, total: 42.20, fulfilled: false),
-  Order(id: '07', orderNumber: '1234-44-339', date: DateTime(2020, 10, 19), productsAndAmount: {sampleProducts[3]: 2, sampleProducts[6]: 1}, total: 19.20, fulfilled: false),
-  Order(id: '08', orderNumber: '1234-44-340', date: DateTime(2020, 10, 20), productsAndAmount: {sampleProducts[3]: 1, sampleProducts[4]: 2}, total: 10.80, fulfilled: false),
-  Order(id: '09', orderNumber: '1234-44-341', date: DateTime(2020, 10, 20), productsAndAmount: {sampleProducts[2]: 2, sampleProducts[0]: 1}, total: 59.30, fulfilled: false),
-  Order(id: '10', orderNumber: '1234-44-342', date: DateTime(2020, 10, 20), productsAndAmount: {sampleProducts[8]: 2, sampleProducts[1]: 1}, total: 69.19, fulfilled: false),
+  Order(id: '00', orderNumber: '123444332', date: DateTime(2020, 10, 9), productsAndAmount: {sampleProducts[0]: 4}, total: 120.21, fulfilled: true),
+  Order(id: '01', orderNumber: '123444333', date: DateTime(2020, 10, 10), productsAndAmount: {sampleProducts[3]: 2, sampleProducts[4]: 1}, total: 17.38, fulfilled: true),
+  Order(id: '02', orderNumber: '123444334', date: DateTime(2020, 10, 10), productsAndAmount: {sampleProducts[7]: 1, sampleProducts[1]: 3}, total: 86.45, fulfilled: true),
+  Order(id: '03', orderNumber: '123444335', date: DateTime(2020, 10, 11), productsAndAmount: {sampleProducts[2]: 2, sampleProducts[0]: 1}, total: 60.49, fulfilled: true),
+  Order(id: '04', orderNumber: '123444336', date: DateTime(2020, 10, 13), productsAndAmount: {sampleProducts[1]: 1, sampleProducts[2]: 3}, total: 38.48, fulfilled: false),
+  Order(id: '05', orderNumber: '123444337', date: DateTime(2020, 10, 13), productsAndAmount: {sampleProducts[0]: 1, sampleProducts[8]: 1}, total: 58.39, fulfilled: true),
+  Order(id: '06', orderNumber: '123444338', date: DateTime(2020, 10, 14), productsAndAmount: {sampleProducts[5]: 1, sampleProducts[6]: 2}, total: 42.20, fulfilled: false),
+  Order(id: '07', orderNumber: '123444339', date: DateTime(2020, 10, 19), productsAndAmount: {sampleProducts[3]: 2, sampleProducts[6]: 1}, total: 19.20, fulfilled: false),
+  Order(id: '08', orderNumber: '123444340', date: DateTime(2020, 10, 20), productsAndAmount: {sampleProducts[3]: 1, sampleProducts[4]: 2}, total: 10.80, fulfilled: false),
+  Order(id: '09', orderNumber: '123444341', date: DateTime(2020, 10, 20), productsAndAmount: {sampleProducts[2]: 2, sampleProducts[0]: 1}, total: 59.30, fulfilled: false),
+  Order(id: '10', orderNumber: '123444342', date: DateTime(2020, 10, 20), productsAndAmount: {sampleProducts[8]: 2, sampleProducts[1]: 1}, total: 69.19, fulfilled: false),
 ];
 
 
