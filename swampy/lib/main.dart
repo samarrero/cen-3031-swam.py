bbdee36867d3ddacaa2a3776020bb0f709f72dbd
Samuel Marrero <helicopter1339@gmail.com>
2020-11-17 21:02:22 -0500

    main.dart reads db products/orders collections and serializes docs into objects

--- swampy/lib/main.dart
+++ swampy/lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:swampy/models/order.dart';
+import 'package:swampy/models/product.dart';
 import 'package:swampy/router/route_names.dart';
 import 'package:swampy/router/router.dart';
 import 'package:swampy/services/firebase_auth_service.dart';
 import 'package:swampy/style.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'data/data.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp().then((v) => null);
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  getFirebaseCollection();
   FluroRouter.setupRouter();
   runApp(
       // Swampy()
@@ -19,12 +26,41 @@ void main() {
             ),
             StreamProvider(
               create: (context) => context.read<FirebaseAuthService>().onAuthStateChanged,
-            )
+            ),
           ],
           child: Swampy()
       )
   );
 }
+Future getFirebaseCollection() async {
+  QuerySnapshot querySnapshotProducts = await FirebaseFirestore.instance.collection('products').get();
+  
+  for (int i = 0; i < querySnapshotProducts.docs.length; i++) {
+    var doc = querySnapshotProducts.docs[i];
+    var data = doc.data();
+    productsList.add(new Product(id: data['id'], name: data['name'], vendor: data['vendor'], price: double.parse(data['price']), amountInInventory: int.parse(data['inventory']), type: data['type'], amountSold: int.parse(data['amount_sold'])));
+  }
+
+  QuerySnapshot querySnapshotOrders = await FirebaseFirestore.instance.collection('orders').get();
+
+  for (int i = 0; i < querySnapshotOrders.docs.length; i++) {
+    var doc = querySnapshotOrders.docs[i];
+    var data = doc.data();
+
+    ordersList.add(new Order(id: data['id'], orderNumber: data['order_number'], date: reformatDateTime(data['date']), productsAndAmount: {productsList[3]: 2, productsList[4]: 1}, total: double.parse(data['total']), fulfilled: data['fulfilled'] == 'TRUE' ? true : false));
+  }
+
+}
+
+DateTime reformatDateTime(String s) {
+  var inputFormat = DateFormat('MM/dd/yyyy');
+  var date1 = inputFormat.parse(s);
+
+  var outputFormat = DateFormat('yyyy-MM-dd');
+  return outputFormat.parse('$date1');
+}
+
+
 
 class Swampy extends StatelessWidget {
   @override
