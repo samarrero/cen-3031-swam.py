e9499d413aa453dc9152c31126abafcbfabdc3b0
Samuel Marrero <helicopter1339@gmail.com>
2020-11-19 22:49:42 -0500

    fixed #2 with if (snapshot.hasData)

--- swampy/lib/pages/product_page/product_page_desktop.dart
+++ swampy/lib/pages/product_page/product_page_desktop.dart
@@ -49,45 +49,77 @@ class ProductPageDesktop extends StatelessWidget {
                               future: FirebaseFirestore.instance.collection('products').doc(ModalRoute.of(context).settings.name.substring(9)).get(),
                               builder: (context, snapshot) {
                                 print(ModalRoute.of(context).settings.name.substring(9));
-                                return Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                          padding:
-                                          EdgeInsets.symmetric(vertical: 7.0)),
-                                      Text(snapshot.data['name'],
-                                          style:
-                                          Theme.of(context).textTheme.headline4.copyWith(fontWeight: FontWeight.bold),
-                                          textAlign: TextAlign.justify),
 
-                                      Padding(
-                                          padding: EdgeInsets.fromLTRB(
-                                              0.0, 7.0, 0.0, 0.0)),
-                                      Text('Price: \$' + snapshot.data['price'],
-                                          // order.date.month.toString() + "/" + order.date.day.toString() + "/" + order.date.year.toString()
-                                          style:
-                                          Theme.of(context).textTheme.headline5,
-                                          textAlign: TextAlign.justify),
-                                      Text('Vendor: ' + snapshot.data['vendor'],
-                                          style:
-                                          Theme.of(context).textTheme.headline5,
-                                          textAlign: TextAlign.justify),
-                                      Text('Type: ' + snapshot.data['type'],
-                                          style:
-                                          Theme.of(context).textTheme.headline5,
-                                          textAlign: TextAlign.justify),
-                                      Text('Current Inventory: ' + snapshot.data['inventory'],
-                                          style:
-                                          Theme.of(context).textTheme.headline5,
-                                          textAlign: TextAlign.justify),
-                                      Text('Inventory Ordered: ' + snapshot.data['amount_sold'],
-                                          style:
-                                          Theme.of(context).textTheme.headline5,
-                                          textAlign: TextAlign.justify),
-                                      Padding(
-                                          padding:
-                                          EdgeInsets.symmetric(vertical: 7.0)),
-                                    ]);
+                                if (snapshot.hasData) {
+                                  return Column(
+                                      crossAxisAlignment: CrossAxisAlignment
+                                          .start,
+                                      children: [
+                                        Padding(
+                                            padding:
+                                            EdgeInsets.symmetric(
+                                                vertical: 7.0)),
+                                        Text(snapshot.data['name'],
+                                            style:
+                                            Theme
+                                                .of(context)
+                                                .textTheme
+                                                .headline4
+                                                .copyWith(
+                                                fontWeight: FontWeight.bold),
+                                            textAlign: TextAlign.justify),
+
+                                        Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                0.0, 7.0, 0.0, 0.0)),
+                                        Text('Price: \$' +
+                                            snapshot.data['price'],
+                                            // order.date.month.toString() + "/" + order.date.day.toString() + "/" + order.date.year.toString()
+                                            style:
+                                            Theme
+                                                .of(context)
+                                                .textTheme
+                                                .headline5,
+                                            textAlign: TextAlign.justify),
+                                        Text('Vendor: ' +
+                                            snapshot.data['vendor'],
+                                            style:
+                                            Theme
+                                                .of(context)
+                                                .textTheme
+                                                .headline5,
+                                            textAlign: TextAlign.justify),
+                                        Text('Type: ' + snapshot.data['type'],
+                                            style:
+                                            Theme
+                                                .of(context)
+                                                .textTheme
+                                                .headline5,
+                                            textAlign: TextAlign.justify),
+                                        Text('Current Inventory: ' +
+                                            snapshot.data['inventory'],
+                                            style:
+                                            Theme
+                                                .of(context)
+                                                .textTheme
+                                                .headline5,
+                                            textAlign: TextAlign.justify),
+                                        Text('Inventory Ordered: ' +
+                                            snapshot.data['amount_sold'],
+                                            style:
+                                            Theme
+                                                .of(context)
+                                                .textTheme
+                                                .headline5,
+                                            textAlign: TextAlign.justify),
+                                        Padding(
+                                            padding:
+                                            EdgeInsets.symmetric(
+                                                vertical: 7.0)),
+                                      ]);
+                                }
+
+                                else return Container();
                               }
                             ),
                             // Padding(
