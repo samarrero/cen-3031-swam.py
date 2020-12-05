d12d363d90da08cba35f7c725e0c87e41b0fc3cc
Fabio Suarez <fabiosuarez.o0@gmail.com>
2020-12-04 20:57:57 -0500

    home page better

--- swampy/lib/pages/home_page.dart
+++ swampy/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:swampy/components/list/list_element.dart';
@@ -11,14 +12,20 @@ import 'package:swampy/services/firebase_auth_service.dart';
 class HomePage extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   List<ListElement> sample = List.generate(25, (index) => ListElement(items: ['$index', '${index + 1}', '${index + 2}', '${index + 3}'],));
-  final GlobalKey<FormState> createAccountKey = GlobalKey<FormState>();
-  final GlobalKey<FormState> loginKey = GlobalKey<FormState>();
-  final Map<String, TextEditingController> controllers = {
-    'first' : TextEditingController(),
-    'last' : TextEditingController(),
-    'email' : TextEditingController(),
-    'password' : TextEditingController()
-  };
+
+  Future<List<dynamic>> getTopProductsAndRecentOrders() async {
+    var top5 = await FirebaseFirestore.instance.collection('products').orderBy('amount_sold', descending: true).limit(5).get();
+    var recentOrders = await FirebaseFirestore.instance.collection('orders').orderBy('date', descending: true).limit(3).get();
+    var top5List = [];
+    var recentOrdersList = [];
+    for (var doc in top5.docs) {
+      top5List.add(doc.data());
+    }
+    for (var doc in recentOrders.docs) {
+      recentOrdersList.add(doc.data());
+    }
+    return [top5List, recentOrdersList];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,10 +40,17 @@ class HomePage extends StatelessWidget {
                     overscroll.disallowGlow();
                     return;
                   },
-                  child: ScreenTypeLayout(
-                    desktop: HomePageDesktop(sample: sample),
-                    tablet: HomePageTablet(sample: sample),
-                    mobile: HomePageMobile(sample: sample),
+                  child: FutureBuilder(
+                    future: getTopProductsAndRecentOrders(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ScreenTypeLayout(
+                          desktop: HomePageDesktop(topProducts: snapshot.data[0], recentOrders: snapshot.data[1]),
+                          tablet: HomePageTablet(sample: sample),
+                          mobile: HomePageMobile(sample: sample),
+                        );
+                      } else return Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(Theme.of(context).primaryColor)));
+                    },
                   )
               )
           ),
--- swampy/lib/pages/home_page/home_page_desktop.dart
+++ swampy/lib/pages/home_page/home_page_desktop.dart
@@ -4,14 +4,21 @@ import 'package:swampy/components/list/list_element.dart';
 import 'package:swampy/components/menus/nav_bar.dart';
 import 'package:swampy/components/menus/side_menu.dart';
 import 'package:swampy/components/general/section.dart';
+import 'package:swampy/data/data.dart';
+import 'package:swampy/models/order.dart';
+import 'package:swampy/router/route.dart';
 
 class HomePageDesktop extends StatelessWidget {
-  final List<ListElement> sample;
+  final List<dynamic> topProducts;
+  final List<dynamic> recentOrders;
 
-  HomePageDesktop({this.sample});
+  HomePageDesktop({this.topProducts, this.recentOrders});
 
   @override
   Widget build(BuildContext context) {
+    List<Order> sortedOrders = List.from(ordersList);
+    sortedOrders.sort((a, b) => a.date.isBefore(b.date) ? 1 : -1);
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(70.0),
@@ -29,103 +36,68 @@ class HomePageDesktop extends StatelessWidget {
             ),
             Container(
               width: MediaQuery.of(context).size.width - 200,
-              child: Section(
-                  title: 'Top Performing Products',
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Card(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(32.0)),
-                            elevation: 3.0,
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 15.0)),
-                                  Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: 7.0)),
-                                        Text('insert product name',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline4
-                                                .copyWith(
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                            textAlign: TextAlign.justify),
-                                        Padding(
-                                            padding: EdgeInsets.fromLTRB(
-                                                0.0, 7.0, 0.0, 0.0)),
-                                        Text('Price: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline5,
-                                            textAlign: TextAlign.justify),
-                                        Text('Vendor: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline5,
-                                            textAlign: TextAlign.justify),
-                                        Text('Number of Orders: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline5,
-                                            textAlign: TextAlign.justify),
-                                        Text('Total Revenue: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline5,
-                                            textAlign: TextAlign.justify),
-                                        Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: 7.0)),
-                                      ]),
-                                ]),
-                          ),
-                          Card(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Section(
+                        title: 'Top Performing Products',
+                        child: Row(
+                          children: [
+                            Card(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(24.0)),
+                              elevation: 3.0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(this.topProducts[0]['name'], style: Theme.of(context).textTheme.headline4.copyWith(fontWeight: FontWeight.bold)),
+                                    Text('Price: \$${this.topProducts[0]['price']}', style: Theme.of(context).textTheme.headline5),
+                                    Text('Vendor: ${this.topProducts[0]['vendor']}', style: Theme.of(context).textTheme.headline5),
+                                    Text('Orders: ${this.topProducts[0]['amount_sold']}', style: Theme.of(context).textTheme.headline5),
+                                    Text('Revenue: \$${this.topProducts[0]['price'] * this.topProducts[0]['amount_sold']}', style: Theme.of(context).textTheme.headline5),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Card(
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(32.0)),
                               elevation: 3.0,
-                              child: Text('Pie chart thing'))
-                        ],
-                      )
-                      // Container(
-                      //   title:'Recent Orders',
-                      //   width: MediaQuery.of(context).size.width - 200,
-                      //   child: ListWrapper(
-                      //     searchType: 'n order',
-                      //     titles: [
-                      //       'Order #',
-                      //       'Date',
-                      //       'Amount',
-                      //       'Total',
-                      //       'Status'
-                      //     ],
-                      //       filterSliders: [2, 3],
-                      //       filterCategories: [4],
-                      //    elements: orders.map((order) => ListElement(
-                      //   route: OrderRoute + order.id,
-                      //   object: order,
-                      //   items: [
-                      //     order.orderNumber,
-                      //     TODO: SORTING NUMERICAL VALUES ARE INCORRECT, SORTING BY STRING INSTEAD
-                      //     order.date.month.toString() + "/" + order.date.day.toString() + "/" + order.date.year.toString(),
-                      //     order.getAmount().toString(),
-                      //     "\$" + order.total.toString(),
-                      //     order.fulfilled ? 'Fulfilled' : 'Pending'
-                      //   ],
-                      //    )).toList(),
-                      //   ),
-                      // ),
-                    ],
-                  )),
+                              child: Container(
+                                color: Colors.red,
+                                width: 300,
+                                height: 300,
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                    Section(
+                        title: 'Recent Orders',
+                        child: ColumnBuilder(
+                          itemCount: 3,
+                          itemBuilder: (context, index) {
+                            return ListElement(
+                              route: OrderRoute + sortedOrders[index].id,
+                              object: sortedOrders[index],
+                              items: [
+                                sortedOrders[index].orderNumber.toString(),
+                                //TODO: SORTING NUMERICAL VALUES ARE INCORRECT, SORTING BY STRING INSTEAD
+                                sortedOrders[index].date.month.toString() + "/" + sortedOrders[index].date.day.toString() + "/" + sortedOrders[index].date.year.toString(),
+                                sortedOrders[index].getAmount().toString(),
+                                "\$" + sortedOrders[index].total.toString(),
+                                sortedOrders[index].fulfilled ? 'Fulfilled' : 'Pending'
+                              ],
+                            );
+                          },
+                        )
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
