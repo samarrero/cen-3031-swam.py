4cef81a49390467b8af24d90b3f38ad5ce4642e7
Samuel Marrero <helicopter1339@gmail.com>
2020-12-08 20:12:14 -0500

    Update product_page_desktop.dart
    individual product card wrapped in a Section rather than a SingleChildScrollView to add title and margins

--- swampy/lib/pages/product_page/product_page_desktop.dart
+++ swampy/lib/pages/product_page/product_page_desktop.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:swampy/components/general/column_builder.dart';
+import 'package:swampy/components/general/section.dart';
 import 'package:swampy/components/list/list_element.dart';
 import 'package:swampy/components/menus/nav_bar.dart';
 import 'package:swampy/components/menus/side_menu.dart';
@@ -32,9 +33,9 @@ class ProductPageDesktop extends StatelessWidget {
             ),
             Container(
               width: MediaQuery.of(context).size.width - 200,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
+              child: Section(
+                  title: 'Products',
+                  child:
                     Card(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(32.0)),
@@ -63,10 +64,9 @@ class ProductPageDesktop extends StatelessWidget {
                             //     ]),
                           ]),
                     ),
-                  ],
+
                 ),
               ),
-            ),
           ],
         ),
       ),
