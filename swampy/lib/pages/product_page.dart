5ae5246d2f154c69121a6600fc012caa0e8a923c
Fabio Suarez <fabiosuarez.o0@gmail.com>
2020-12-08 21:39:15 -0500

    redesigned look for product page card, started working on editing

--- swampy/lib/pages/product_page.dart
+++ swampy/lib/pages/product_page.dart
@@ -72,7 +72,7 @@ class _ProductDescriptorState extends State<ProductDescriptor> {
           if (user == null) {
             return StaticProductDescriptor(document: widget.document);
           } else {
-            return EditableProductDescriptor();
+            return EditableProductDescriptor(document: widget.document);
           }
         }
     );
@@ -86,103 +86,49 @@ class StaticProductDescriptor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveBuilder(
-      builder: (context, sizingInformation) {
-        if (!sizingInformation.isDesktop) {
-          return Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(32.0)),
-            elevation: 3.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0, left: 16.0, bottom: 16.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(document['name'],
-                          style: Theme.of(context).textTheme.headline4.copyWith(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 8.0),
-                        Text('Price: \$' + document['price'].toString(),
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Vendor: ' + document['vendor'],
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Type: ' + document['type'],
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Current Inventory: ' + document['inventory'].toString(),
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Inventory Ordered: ' + document['amount_sold'].toString(),
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                      ]),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0, bottom: 16.0, right: 16.0),
-                  child: Text(document['description'], style: Theme.of(context).textTheme.headline5,),
-                ),
-              ],
-            ),
-          );
-        }
-        return Card(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(32.0)),
-          elevation: 3.0,
-          child: Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 16.0, left: 16.0, bottom: 16.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(document['name'],
-                          style: Theme.of(context).textTheme.headline4.copyWith(fontWeight: FontWeight.bold),),
-                        SizedBox(height: 8.0),
-                        Text('Price: \$' + document['price'].toString(),
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Vendor: ' + document['vendor'],
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Type: ' + document['type'],
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Current Inventory: ' + document['inventory'].toString(),
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                        Text('Inventory Ordered: ' + document['amount_sold'].toString(),
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 2.0),
-                      ]),
-                ),
-              ),
-              Flexible(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 16.0, right: 16.0, bottom: 16.0),
-                    child: Text(document['description'], style: Theme.of(context).textTheme.headline5,),
-                  )
-              ),
-            ],
-          ),
-        );
-      },
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32.0)),
+      elevation: 3.0,
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(document['name'],
+              style: Theme.of(context).textTheme.headline4.copyWith(fontWeight: FontWeight.bold),),
+            SizedBox(height: 8.0),
+            Text('Price: \$' + document['price'].toString(),
+                style: Theme.of(context).textTheme.headline5),
+            SizedBox(height: 2.0),
+            Text('Vendor: ' + document['vendor'],
+                style: Theme.of(context).textTheme.headline5),
+            SizedBox(height: 2.0),
+            Text('Type: ' + document['type'],
+                style: Theme.of(context).textTheme.headline5),
+            SizedBox(height: 2.0),
+            Text('Current Inventory: ' + document['inventory'].toString(),
+                style: Theme.of(context).textTheme.headline5),
+            SizedBox(height: 2.0),
+            Text('Inventory Ordered: ' + document['amount_sold'].toString(),
+                style: Theme.of(context).textTheme.headline5),
+            SizedBox(height: 16.0),
+            Text('Description:', style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),),
+            SizedBox(height: 2.0),
+            Text(document['description'], style: Theme.of(context).textTheme.headline5,),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class EditableProductDescriptor extends StatefulWidget {
+  final DocumentSnapshot document;
+
+  EditableProductDescriptor({this.document});
+
   @override
   _EditableProductDescriptorState createState() => _EditableProductDescriptorState();
 }
@@ -190,7 +136,59 @@ class EditableProductDescriptor extends StatefulWidget {
 class _EditableProductDescriptorState extends State<EditableProductDescriptor> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32.0)),
+      elevation: 3.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(widget.document['name'],
+                  style: Theme.of(context).textTheme.headline4.copyWith(fontWeight: FontWeight.bold),),
+                SizedBox(height: 8.0),
+                Text('Price: \$' + widget.document['price'].toString(),
+                    style: Theme.of(context).textTheme.headline5),
+                SizedBox(height: 2.0),
+                Text('Vendor: ' + widget.document['vendor'],
+                    style: Theme.of(context).textTheme.headline5),
+                SizedBox(height: 2.0),
+                Text('Type: ' + widget.document['type'],
+                    style: Theme.of(context).textTheme.headline5),
+                SizedBox(height: 2.0),
+                Text('Current Inventory: ' + widget.document['inventory'].toString(),
+                    style: Theme.of(context).textTheme.headline5),
+                SizedBox(height: 2.0),
+                Text('Inventory Ordered: ' + widget.document['amount_sold'].toString(),
+                    style: Theme.of(context).textTheme.headline5),
+                SizedBox(height: 16.0),
+                Text('Description:', style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),),
+                SizedBox(height: 2.0),
+                Text(widget.document['description'], style: Theme.of(context).textTheme.headline5,),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 32.0, right: 32.0),
+            child: FlatButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text('Edit', style: Theme.of(context).textTheme.button.copyWith(color: Colors.white)),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
