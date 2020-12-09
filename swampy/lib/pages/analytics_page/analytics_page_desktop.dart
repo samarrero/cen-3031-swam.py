b56bd7060fa1136ff8cce9fa5eaa78574137dd6f
Fabio Suarez <fabiosuarez.o0@gmail.com>
2020-12-09 14:17:32 -0500

    better looking analytics desktop

--- swampy/lib/pages/analytics_page/analytics_page_desktop.dart
+++ swampy/lib/pages/analytics_page/analytics_page_desktop.dart
@@ -36,113 +36,126 @@ class AnalyticsPageDesktop extends StatelessWidget {
                 width: MediaQuery.of(context).size.width - 200,
                 child: Section(
                   title: 'Analytics',
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ConstrainedBox(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width - 700
-                        ),
-                        child: SfCartesianChart(
-                            primaryYAxis: NumericAxis(
-                              //Formatting the labels in locale’s currency pattern with symbol.
-                                numberFormat: NumberFormat.currency(
-                                    locale: 'en_US',
-                                    symbol: "\$"
+                  child: Center(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: 1440
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 32.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Flexible(
+                              child: ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  maxWidth: 800
                                 ),
-                            ),
-                            primaryXAxis: CategoryAxis(),
-                            // Chart title
-                            title: ChartTitle(text: 'Total Sales over Time', textStyle: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold)),
-                            // Enable legend
-                            legend: Legend(isVisible: true, position: LegendPosition.bottom),
-                            // Enable tooltip
-                            tooltipBehavior: TooltipBehavior(
-                                enable: true,
-                                color: Colors.grey[800],
-                                textStyle: Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.normal, color: Colors.white)
-                            ),
-                            series: [
-                              AreaSeries(
-                                  gradient: LinearGradient(
-                                      begin: Alignment.topCenter,
-                                      end: Alignment.bottomCenter,
-                                      colors: [Theme.of(context).primaryColor, Color.fromRGBO(212, 225, 255, 1)],//donutColors.values.toList().sublist(2, 5),
-                                      stops: [0, 1],
-                                  ),
-                                  // trendlines: [
-                                  //   Trendline(type: TrendlineType.linear, color: Colors.redAccent, name: "Trendline")
-                                  // ],
-                                  // borderWidth: 1,
-                                  // borderColor: donutColors.values.toList()[2],
-                                  name: 'Revenue',
-                                  color: Theme.of(context).primaryColor,
-                                  dataSource: ordersData,
-                                  xValueMapper: (sales, _) => sales.x,
-                                  yValueMapper: (sales, _) => sales.y,
-                                  // Enable data label
-                                  dataLabelSettings: DataLabelSettings(isVisible: false))
-                            ]),
-                      ),
-                      SfCircularChart(
-                          tooltipBehavior: TooltipBehavior(
-                            enable: true,
-                            color: Colors.grey[800],
-                              textStyle: Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.normal, color: Colors.white)
-                          ),
-                          title: ChartTitle(text: 'Best Performing Products', textStyle: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold)),
-                          annotations: <CircularChartAnnotation>[
-                            CircularChartAnnotation(
-                              widget: ColumnBuilder(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                itemCount: donutData.length,
-                                itemBuilder: (context, index) {
-                                  return ConstrainedBox(
-                                    constraints: BoxConstraints(
-                                        maxWidth: 160
+                                child: SfCartesianChart(
+                                    primaryYAxis: NumericAxis(
+                                      //Formatting the labels in locale’s currency pattern with symbol.
+                                        numberFormat: NumberFormat.currency(
+                                            locale: 'en_US',
+                                            symbol: "\$"
+                                        ),
                                     ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(vertical: 2.5),
-                                      child: Row(
-                                        children: [
-                                          Container(
-                                            decoration: BoxDecoration(
-                                                borderRadius: BorderRadius.circular(100),
-                                                color: donutColors[donutData[index].x]
-                                            ),
-                                            width: 16,
-                                            height: 16,
-                                          ),
-                                          SizedBox(width: 8.0),
-                                          Flexible(
-                                              fit: FlexFit.loose,
-                                              child: Text(donutData[index].x,
-                                                  style: Theme.of(context).textTheme.headline5,
-                                                  overflow: TextOverflow.ellipsis)
-                                          ),
-                                        ],
-                                      ),
+                                    primaryXAxis: CategoryAxis(),
+                                    // Chart title
+                                    title: ChartTitle(text: 'Total Sales over Time', textStyle: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold)),
+                                    // Enable legend
+                                    legend: Legend(isVisible: true, position: LegendPosition.bottom),
+                                    // Enable tooltip
+                                    tooltipBehavior: TooltipBehavior(
+                                        enable: true,
+                                        color: Colors.grey[800],
+                                        textStyle: Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.normal, color: Colors.white)
                                     ),
-                                  );
-                                },
+                                    series: [
+                                      AreaSeries(
+                                          gradient: LinearGradient(
+                                              begin: Alignment.topCenter,
+                                              end: Alignment.bottomCenter,
+                                              colors: [Theme.of(context).primaryColor, Color.fromRGBO(212, 225, 255, 1)],//donutColors.values.toList().sublist(2, 5),
+                                              stops: [0, 1],
+                                          ),
+                                          // trendlines: [
+                                          //   Trendline(type: TrendlineType.linear, color: Colors.redAccent, name: "Trendline")
+                                          // ],
+                                          // borderWidth: 1,
+                                          // borderColor: donutColors.values.toList()[2],
+                                          name: 'Revenue',
+                                          color: Theme.of(context).primaryColor,
+                                          dataSource: ordersData,
+                                          xValueMapper: (sales, _) => sales.x,
+                                          yValueMapper: (sales, _) => sales.y,
+                                          // Enable data label
+                                          dataLabelSettings: DataLabelSettings(isVisible: false))
+                                    ]),
                               ),
-                            )
+                            ),
+                            SizedBox(width: 32.0),
+                            SfCircularChart(
+                                tooltipBehavior: TooltipBehavior(
+                                  enable: true,
+                                  color: Colors.grey[800],
+                                    textStyle: Theme.of(context).textTheme.headline6.copyWith(fontWeight: FontWeight.normal, color: Colors.white)
+                                ),
+                                title: ChartTitle(text: 'Best Performing Products', textStyle: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold)),
+                                annotations: <CircularChartAnnotation>[
+                                  CircularChartAnnotation(
+                                    widget: ColumnBuilder(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      itemCount: donutData.length,
+                                      itemBuilder: (context, index) {
+                                        return ConstrainedBox(
+                                          constraints: BoxConstraints(
+                                              maxWidth: 160
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(vertical: 2.5),
+                                            child: Row(
+                                              children: [
+                                                Container(
+                                                  decoration: BoxDecoration(
+                                                      borderRadius: BorderRadius.circular(100),
+                                                      color: donutColors[donutData[index].x]
+                                                  ),
+                                                  width: 16,
+                                                  height: 16,
+                                                ),
+                                                SizedBox(width: 8.0),
+                                                Flexible(
+                                                    fit: FlexFit.loose,
+                                                    child: Text(donutData[index].x,
+                                                        style: Theme.of(context).textTheme.headline5,
+                                                        overflow: TextOverflow.ellipsis)
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  )
+                                ],
+                                series: <CircularSeries>[
+                                  // Renders doughnut chart
+                                  DoughnutSeries(
+                                    dataSource: donutData,
+                                    // startAngle: 30,
+                                    // endAngle: 30,
+                                    innerRadius: '115',
+                                    radius: '155',
+                                    pointColorMapper:(data,  _) => data.color,
+                                    xValueMapper: (data, _) => data.x,
+                                    yValueMapper: (data, _) => data.y,
+                                  )
+                                ]
+                            ),
                           ],
-                          series: <CircularSeries>[
-                            // Renders doughnut chart
-                            DoughnutSeries(
-                              dataSource: donutData,
-                              // startAngle: 30,
-                              // endAngle: 30,
-                              innerRadius: '115',
-                              radius: '155',
-                              pointColorMapper:(data,  _) => data.color,
-                              xValueMapper: (data, _) => data.x,
-                              yValueMapper: (data, _) => data.y,
-                            )
-                          ]
+                        ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
               )
